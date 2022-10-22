7f0da1604d05bc64ce7d09b8730ae4f2ea271237
Kaiyang Yao <yaoky2008@gmail.com>
2022-10-21 21:58:32 -0500

    add cloudinary feature

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
+    // username and password is handled in the plugin below
     email: {
         type: String,
         required: true,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,33 +4,21 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const { storage } = require('../cloudinary/index');
-const upload = multer({ dest: 'uploads/' });
-const upload2 = multer({ storage });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
-// console.log(upload);
-// console.log(upload2);
-
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(upload2.array('image'), (req, res) => {
-        console.log(req.body, req.files);
-        res.send("Hello");
-    })
-    // .post(upload2.single('image'), (req, res) => {
-    //     console.log(req.body, req.file);
-    //     res.send("Hello");
-    // })
-    // .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
